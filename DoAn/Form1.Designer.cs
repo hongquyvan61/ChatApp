f20043f38ff7831c6b8aa802129eaf5495635d8e
HoHuuCuong <103228514+HoHuuCuong@users.noreply.github.com>
2022-11-27 15:44:36 +0700

    dang ki

--- DoAn/Form1.Designer.cs
+++ DoAn/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.txtpassword = new MaterialSkin.Controls.MaterialTextBox2();
             this.logochat = new System.Windows.Forms.PictureBox();
             this.btndangnhap = new MaterialSkin.Controls.MaterialButton();
+            this.btn_dangki = new MaterialSkin.Controls.MaterialButton();
             ((System.ComponentModel.ISupportInitialize)(this.logochat)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,8 +49,7 @@
             this.txtusername.HideSelection = true;
             this.txtusername.Hint = "Nhập username...";
             this.txtusername.LeadingIcon = null;
-            this.txtusername.Location = new System.Drawing.Point(144, 218);
-            this.txtusername.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtusername.Location = new System.Drawing.Point(165, 291);
             this.txtusername.MaxLength = 32767;
             this.txtusername.MouseState = MaterialSkin.MouseState.OUT;
             this.txtusername.Name = "txtusername";
@@ -61,7 +61,7 @@
             this.txtusername.SelectionLength = 0;
             this.txtusername.SelectionStart = 0;
             this.txtusername.ShortcutsEnabled = true;
-            this.txtusername.Size = new System.Drawing.Size(204, 48);
+            this.txtusername.Size = new System.Drawing.Size(233, 48);
             this.txtusername.TabIndex = 1;
             this.txtusername.TabStop = false;
             this.txtusername.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
@@ -73,7 +73,7 @@
             this.materialLabel3.AutoSize = true;
             this.materialLabel3.Depth = 0;
             this.materialLabel3.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.materialLabel3.Location = new System.Drawing.Point(60, 228);
+            this.materialLabel3.Location = new System.Drawing.Point(69, 304);
             this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel3.Name = "materialLabel3";
             this.materialLabel3.Size = new System.Drawing.Size(76, 19);
@@ -85,7 +85,7 @@
             this.materialLabel4.AutoSize = true;
             this.materialLabel4.Depth = 0;
             this.materialLabel4.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.materialLabel4.Location = new System.Drawing.Point(60, 282);
+            this.materialLabel4.Location = new System.Drawing.Point(69, 376);
             this.materialLabel4.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel4.Name = "materialLabel4";
             this.materialLabel4.Size = new System.Drawing.Size(75, 19);
@@ -102,8 +102,7 @@
             this.txtpassword.HideSelection = true;
             this.txtpassword.Hint = "Nhập password...";
             this.txtpassword.LeadingIcon = null;
-            this.txtpassword.Location = new System.Drawing.Point(144, 272);
-            this.txtpassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtpassword.Location = new System.Drawing.Point(165, 363);
             this.txtpassword.MaxLength = 32767;
             this.txtpassword.MouseState = MaterialSkin.MouseState.OUT;
             this.txtpassword.Name = "txtpassword";
@@ -115,7 +114,7 @@
             this.txtpassword.SelectionLength = 0;
             this.txtpassword.SelectionStart = 0;
             this.txtpassword.ShortcutsEnabled = true;
-            this.txtpassword.Size = new System.Drawing.Size(204, 48);
+            this.txtpassword.Size = new System.Drawing.Size(233, 48);
             this.txtpassword.TabIndex = 4;
             this.txtpassword.TabStop = false;
             this.txtpassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
@@ -125,10 +124,9 @@
             // logochat
             // 
             this.logochat.Image = ((System.Drawing.Image)(resources.GetObject("logochat.Image")));
-            this.logochat.Location = new System.Drawing.Point(123, 70);
-            this.logochat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.logochat.Location = new System.Drawing.Point(141, 93);
             this.logochat.Name = "logochat";
-            this.logochat.Size = new System.Drawing.Size(180, 126);
+            this.logochat.Size = new System.Drawing.Size(206, 168);
             this.logochat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.logochat.TabIndex = 5;
             this.logochat.TabStop = false;
@@ -141,12 +139,12 @@
             this.btndangnhap.Depth = 0;
             this.btndangnhap.HighEmphasis = true;
             this.btndangnhap.Icon = null;
-            this.btndangnhap.Location = new System.Drawing.Point(130, 332);
-            this.btndangnhap.Margin = new System.Windows.Forms.Padding(4);
+            this.btndangnhap.Location = new System.Drawing.Point(260, 444);
+            this.btndangnhap.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btndangnhap.MouseState = MaterialSkin.MouseState.HOVER;
             this.btndangnhap.Name = "btndangnhap";
             this.btndangnhap.NoAccentTextColor = System.Drawing.Color.Empty;
-            this.btndangnhap.Size = new System.Drawing.Size(173, 34);
+            this.btndangnhap.Size = new System.Drawing.Size(138, 45);
             this.btndangnhap.TabIndex = 6;
             this.btndangnhap.Text = "Đăng nhập";
             this.btndangnhap.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
@@ -154,20 +152,40 @@
             this.btndangnhap.UseVisualStyleBackColor = true;
             this.btndangnhap.Click += new System.EventHandler(this.btndangnhap_Click);
             // 
+            // btn_dangki
+            // 
+            this.btn_dangki.AutoSize = false;
+            this.btn_dangki.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn_dangki.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btn_dangki.Depth = 0;
+            this.btn_dangki.HighEmphasis = true;
+            this.btn_dangki.Icon = null;
+            this.btn_dangki.Location = new System.Drawing.Point(69, 444);
+            this.btn_dangki.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_dangki.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btn_dangki.Name = "btn_dangki";
+            this.btn_dangki.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btn_dangki.Size = new System.Drawing.Size(135, 45);
+            this.btn_dangki.TabIndex = 7;
+            this.btn_dangki.Text = "Đăng kí";
+            this.btn_dangki.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btn_dangki.UseAccentColor = false;
+            this.btn_dangki.UseVisualStyleBackColor = true;
+            this.btn_dangki.Click += new System.EventHandler(this.btn_dangki_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(430, 416);
+            this.ClientSize = new System.Drawing.Size(491, 555);
+            this.Controls.Add(this.btn_dangki);
             this.Controls.Add(this.btndangnhap);
             this.Controls.Add(this.logochat);
             this.Controls.Add(this.txtpassword);
             this.Controls.Add(this.materialLabel4);
             this.Controls.Add(this.materialLabel3);
             this.Controls.Add(this.txtusername);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
-            this.Padding = new System.Windows.Forms.Padding(3, 48, 3, 2);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Đăng nhập";
             ((System.ComponentModel.ISupportInitialize)(this.logochat)).EndInit();
@@ -187,5 +205,6 @@
         private MaterialSkin.Controls.MaterialTextBox2 txtpassword;
         private PictureBox logochat;
         private MaterialSkin.Controls.MaterialButton btndangnhap;
+        private MaterialSkin.Controls.MaterialButton btn_dangki;
     }
 }
